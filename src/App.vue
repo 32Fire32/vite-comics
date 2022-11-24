c3b64e0459078b8ffa431ea191e5e3f546792a5f
32Fire32 <nicola.gianfelici@hotmail.it>
2022-11-24 15:12:43 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import AppTitle from './components/AppTitle.vue'
-import AppHero from './components/AppHero.vue'
+<script>
+import AppMain from './components/AppMain.vue'
+import AppHeader from './components/AppHeader.vue'
+import AppFooter from './components/AppFooter.vue'
+export default {
+    components: {
+        AppHeader,
+        AppMain,        
+        AppFooter
+    }
+}
 </script>
 
 <template>
-    <AppTitle />
-    <AppHero />  
+    <AppHeader/>
+    <AppMain />
+    <AppFooter />  
 </template>
 
 <style scoped>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,12 @@
+<script >
+export default {
+  name: 'AppHeader',
+}
+</script>
+
+<template>
+    <img src="../assets/" alt="game">
+</template>
+
+<style scoped>
+</style>
